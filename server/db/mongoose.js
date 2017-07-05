9fc4f58bb85fd30a4a1bf77a2a152c538ab3e74d
Shrekie <thomaslindauer@mail.com>
2017-07-05 19:52:06 +0200

    new user

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //set promise library to global
-mongoose.connect('mongodb://testaccount:testpassword123@ds149132.mlab.com:49132/todoapp-shrek');
+mongoose.connect('mongodb://testaccountdb:testaccountdb123@ds149132.mlab.com:49132/todoapp-shrek');
 
 module.exports = {mongoose};
