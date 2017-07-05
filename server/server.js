fd6fcdf419362eb5611b4c531c7426cb568741d3
Shrekie <thomaslindauer@mail.com>
2017-07-05 21:30:00 +0200

    added patched and lodash package

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const _ = require('lodash');
+const express = require('express')
+const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
@@ -65,6 +66,31 @@ app.delete('/todos/:id',(req, res)=>{
 		return res.status(400).send();
 	});
 
+});
+
+app.patch('/todos/:id', (req, res)=>{
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	} else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e)=>{
+		res.status(400).send();
+	})
 });
 
 app.listen(port, () =>{
